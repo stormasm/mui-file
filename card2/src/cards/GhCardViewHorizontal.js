26927c2af73ab108265f7c2254b82d7b000ebaec
Michael Angerman <stormasm@gmail.com>
2018-07-28 15:03:57 -0700

    change the background color to grey

--- card2/src/cards/GhCardViewHorizontal.js
+++ card2/src/cards/GhCardViewHorizontal.js
@@ -4,12 +4,12 @@ import { withStyles } from "@material-ui/core/styles";
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
 import GhCard from "./../cards/GhCard";
-import lightGreen from "@material-ui/core/colors/lightGreen";
+import grey from "@material-ui/core/colors/grey";
 
 const styles = theme => ({
   gridListSingleLine: {
     flexWrap: "nowrap",
-    backgroundColor: lightGreen[100]
+    backgroundColor: grey[50]
   }
 });
 
@@ -18,17 +18,17 @@ function GhCardViewHorizontal(props) {
 
   return (
     <div>
-        <GridList
-          cellHeight={250}
-          className={classes.gridListSingleLine}
-          cols={6}
-        >
-          {tileData.map(tile => (
-            <GridListTile key={tile.avatar}>
-              <GhCard tile={tile} />
-            </GridListTile>
-          ))}
-        </GridList>
+      <GridList
+        cellHeight={250}
+        className={classes.gridListSingleLine}
+        cols={6}
+      >
+        {tileData.map(tile => (
+          <GridListTile key={tile.avatar}>
+            <GhCard tile={tile} />
+          </GridListTile>
+        ))}
+      </GridList>
     </div>
   );
 }
